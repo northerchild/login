fdcf94bd6d32b78ccc4662dd0a8f46e00d75c0bc
Northerchild <osarus-13@hotmail.com>
2018-07-06 14:27:53 -0500

    Sección principal inicio prueba

--- views/principalView.php
+++ views/principalView.php
@@ -5,9 +5,36 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/estilos.css">
-	<title>Bienvenido</title>
+	<title>Sesion iniciada</title>
 </head>
 <body>
-
+	<style>
+        
+        body{
+            background-image: url(image/bg2.jpeg);
+        }
+        
+        .welcome{
+            width: 100%;
+            max-width: 600px;
+            margin: auto;
+            margin-top: 100px;
+            background: rgba(0,0,0,0.6);
+            text-align: center;
+            padding: 20px;
+        }
+        
+        .welcome img{
+            width: 120px;
+            height: 120px;
+            text-align: center;
+        }
+    
+    </style>
+    <div class="welcome">
+       <img src="image/logo_magtimus.png" >
+        <h1>Bienvenido, has logrado iniciar sesion</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+   </div>
 </body>
 </html>
